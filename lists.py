eccec4dfe6198ef4ca3fa6a23253622cf33cd849
Shoh8228 <120650519+Shoh8228@users.noreply.github.com>
2023-02-06 22:12:24 +0500

    Yangi fayllar qo'shildi.

---
+++ lists.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  3 22:36:14 2023
+
+@author: admin
+"""
+
+# mevalar = ['olma', 'anjir', 'shaftoli', "o'rik"]
+# narhlar = [12000, 18000, 10900, 22000]
+# sonlar = ['bir', 'ikki', 3, 4, 5]
+# ismlar = []
+
+# print("Oxirgi meva: ", mevalar[3])
+# print("Ikkinchi meva: ", mevalar[1])
+
+# print("Birinchi meva: ", mevalar[0].title())
+# print("Ikkinchi meva: ", mevalar[1].upper())
+
+# print(narhlar[1]+narhlar[3])
+
+# car_model = ['Toyota', 'GM', 'BMW', 'Hyundai', "Kia", "Volkswagen"]
+# print(car_model[-1])
+
+# mevalar[0] = 'anor'
+# print(mevalar)
+
+# mevalar.append('anor')
+# mevalar.append('tarvuz')
+# print(mevalar)
+
+# cars = ['Lacetti', 'Nexia 3', 'Cobalt']
+# cars.insert(0, 'Damas')
+# cars.insert(3, 'Malibu')
+# print(cars)
+
+# cars = []
+# cars.append("Lacetti")
+# cars.append("Nexia 3")
+# cars.append("Cobalt")
+# cars.insert(0, 'Malibu')
+# cars.insert(2, "Tracker")
+# print(cars)
+# del cars[3]
+# print(cars)
+# cars.remove('Cobalt')
+# print(cars)
+
+# hayvonlar = ['it', 'mushuk', 'sigir', "qo'y", "quyon", 'mushuk']
+# hayvonlar.remove('mushuk')
+# print(hayvonlar)
+
+# bozorlik = ["yog'", "un", "piyoz", "banan", "go'sht"]
+# mahsulot = bozorlik.pop(3)
+# print("Men " + mahsulot + " sotib oldim.")
+# print("Olinmagan mahsulotlar: ", bozorlik)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
